0dd1df88af38e7661fe84da56bf34c5734a61d92
redacted2001 <bnj5@usca.edu>
2023-11-15 12:49:12 -0500

    11152023

--- js/hotel.js
+++ js/hotel.js
@@ -14,3 +14,37 @@ console.log(test.booked);
 test.booked = 155;
 
 console.log(test.booked);
+
+//start hotel res
+var grouplist = [];
+function reservation(){
+    //get vals and put in object
+    var guest = {
+        name: $('#name').val(),
+        gnumber: $('#num').val(),
+        checkin: $('#checkin').val(),
+        checkout: $('#checkout').val(),
+        roomtype: $('#room').val()
+    };
+    grouplist.push(guest);
+    console.log(guest);
+}
+
+function displayL(){
+    $('#guestlist').html("");
+    $('#guestlist').append("<table>");
+    for (var i in grouplist){
+        $("#guestlist").append("<tr><td>"+grouplist[i].name+"</td>");
+        $("#guestlist").append("<td>"+grouplist[i].gnumber+"</td>");
+        $("#guestlist").append("<td>"+grouplist[i].checkin+"</td>");
+        $("#guestlist").append("<td>"+grouplist[i].checkout+"</td>");
+        $("#guestlist").append("<td>"+grouplist[i].roomtype+"</td></tr>");
+    };
+    $("#guestlist").append("</table>");
+}
+
+//list handler
+$('#list').click(displayL);
+
+//register event handler
+$('#submit').click(reservation);
